3f670aeb6786c1bf3cc8f642b6d756d7167d5d5f
jinblo <katt406@gmail.com>
2023-10-23 13:16:41 +0300

    init

---
+++ src/main/java/s23/team1project/Team1projectApplication.java
@@ -0,0 +1,13 @@
+package s23.team1project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s23/team1project/Team1projectApplicationTests.java
@@ -0,0 +1,13 @@
+package s23.team1project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
