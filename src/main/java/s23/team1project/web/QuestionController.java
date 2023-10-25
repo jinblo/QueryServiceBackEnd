d5ffaff0064aa4835b67610a95a1998ff3bc8c67
jinblo <katt406@gmail.com>
2023-10-25 13:45:22 +0300

    question controller

--- src/main/java/s23/team1project/web/QuestionController.java
+++ src/main/java/s23/team1project/web/QuestionController.java
@@ -4,38 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import s23.team1project.domain.Query;
 import s23.team1project.domain.QueryRepository;
+import s23.team1project.domain.Question;
 import s23.team1project.domain.QuestionRepository;
 
 @Controller
-public class QueryController {
-
+public class QuestionController {
+	
 	@Autowired
 	private QuestionRepository questionRepo;
 	@Autowired
 	private QueryRepository queryRepo;
 	
-	@GetMapping("index")
-	public String index(Model model) {
-		model.addAttribute("queries", queryRepo.findAll());
-		return "index";
+	@GetMapping("query/{id}/addquestion")
+	public String addquestion(@PathVariable("id") Long queryId,Model model) {
+		model.addAttribute("query", queryRepo.findById(queryId));
+		model.addAttribute("question", new Question());
+		return "addquestion";
 	}
 	
-	@GetMapping("addquery")
-	public String addQuery(Model model) {
-		model.addAttribute("query", new Query());
-		return "addquery";
-	}
-	
-	@PostMapping("savequery")
-	public String saveQuery(Query query) {
-		queryRepo.save(query);
+	@PostMapping("query/{id}/savequestion")
+	public String savequestion(Question question) {
+		questionRepo.save(question);
 		return "redirect:index";
 	}
-	
 
-	
 }
