cd76a15206db15767b33b6c76717ce52c94c0634
toivoantero <125696622+toivoantero@users.noreply.github.com>
2023-10-30 12:45:28 +0200

    Rest get queres, eka kokeilu

--- src/main/java/s23/team1project/web/QueryController.java
+++ src/main/java/s23/team1project/web/QueryController.java
@@ -1,10 +1,15 @@
 package s23.team1project.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import s23.team1project.domain.Query;
 import s23.team1project.domain.QueryRepository;
@@ -36,6 +41,9 @@ public class QueryController {
 		return "redirect:index";
 	}
 	
-
+	@RequestMapping(value="/queries", method = RequestMethod.GET)
+    public @ResponseBody List<Query> queryListRest() {	
+        return (List<Query>) queryRepo.findAll();
+    }
 	
 }
