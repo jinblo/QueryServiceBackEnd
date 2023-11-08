f3e6690c4001cc04d75b41ee88cb98ecc7b28ca2
jinblo <katt406@gmail.com>
2023-11-08 11:22:37 +0200

    crossorigin to restcontroller

--- src/main/java/s23/team1project/web/QueryRestController.java
+++ src/main/java/s23/team1project/web/QueryRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import s23.team1project.domain.QueryRepository;
 import s23.team1project.domain.Question;
 import s23.team1project.domain.QuestionRepository;
 
+@CrossOrigin
 @RestController
 public class QueryRestController {
 	
