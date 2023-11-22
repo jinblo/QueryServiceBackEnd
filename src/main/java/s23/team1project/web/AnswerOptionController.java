5ace89b495235caf5345201f23144ce70ce747e1
jinblo <katt406@gmail.com>
2023-11-22 13:15:20 +0200

    answeroption template

--- src/main/java/s23/team1project/web/AnswerOptionController.java
+++ src/main/java/s23/team1project/web/AnswerOptionController.java
@@ -2,8 +2,16 @@ package s23.team1project.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import s23.team1project.domain.AnswerOption;
 import s23.team1project.domain.AnswerOptionRepository;
+import s23.team1project.domain.Query;
+import s23.team1project.domain.QueryRepository;
+import s23.team1project.domain.Question;
 import s23.team1project.domain.QuestionRepository;
 
 @Controller
@@ -14,5 +22,20 @@ public class AnswerOptionController {
 	@Autowired
 	private AnswerOptionRepository answerOptionRepo;
 	
+	// Adding AnswerOption
+	@GetMapping("query/{queryId}/question/{questionId}/addansweroption")
+	public String addQuestion(@PathVariable("questionId") Long questionId, Model model) {
+		model.addAttribute("question", questionRepo.findById(questionId));
+		model.addAttribute("answerOption", new AnswerOption());
+		return "addansweroption";
+	}
+	
+	// Saving AnswerOption
+	@PostMapping("query/{queryId}/question/{questionId}/saveansweroption")
+	public String saveAnswerOption(@PathVariable("questionId") Long questionId, AnswerOption answerOption) {
+		Question question = questionRepo.findById(questionId).get();
+		answerOptionRepo.save(answerOption);
+		return "redirect:../../../{queryId}/listquestions";
+	}
 	
 }
